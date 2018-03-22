23c6a7fcef8732a114555adf1a4196768af61e03
Terna <tkpamber@yahoo.com>
2018-03-22 11:04:06 -0700

    use stereo cameras

--- src/apps/plane.cpp
+++ src/apps/plane.cpp
@@ -12,28 +12,42 @@ int main(int argc, char* args[]) {
   OrbFeatures matcher;
   TickMeter tm;
   
-  cv::Mat frame0 = cv::imread(IMAGES_DIR + "aloe_l.jpg");
-  cv::Mat frame1 = cv::imread(IMAGES_DIR + "aloe_r.jpg");
+  cv::VideoCapture cam1(0);
+  cam1.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+  cam1.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
   
-//  cv::resize(frame0, frame0, frame0.size() / 2);
-//  cv::resize(frame1, frame1, frame0.size());
+  cv::VideoCapture cam2(1);
+  cam2.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+  cam2.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
   
-  OrbFeatures::Detection prev = matcher.detect(frame0);
-  OrbFeatures::Detection curr = matcher.detect(frame1);
+  for(;;) {
+    cv::Mat frame1;// = cv::imread(IMAGES_DIR + "aloe_l.jpg");
+    cv::Mat frame2;// = cv::imread(IMAGES_DIR + "aloe_r.jpg");
+    
+    cam1 >> frame1;
+    cam2 >> frame2;
+    
+  //  cv::resize(frame0, frame0, frame0.size() / 2);
+  //  cv::resize(frame1, frame1, frame0.size());
+    
+    OrbFeatures::Detection feat1 = matcher.detect(frame1);
+    OrbFeatures::Detection feat2 = matcher.detect(frame2);
 
-  OrbFeatures::Match match_results = matcher.match(prev, curr);
-  
-  vector<bool> mask = OrbFeatures::median_filter_matches(match_results);
-  
-//  features::draw_points(frame1, match_results.outliers, Scalar(0,0,255));
-  features::draw_history(frame1, match_results.matched_src, match_results.matched, mask);
-  
-//  cvtColor(frame0, frame0, CV_BGR2GRAY);
-//  cvtColor(frame1, frame1, CV_BGR2GRAY);
-//  Ptr<StereoMatcher> stereo = StereoBM::create();
-//  cv::Mat disparity;
-//  stereo->compute(frame0, frame1, disparity);
-  
-  imshow("orb_tracking", frame1);
-  cv::waitKey();
+    OrbFeatures::Match match_results = matcher.match(feat1, feat2);
+    
+    vector<bool> mask = OrbFeatures::median_filter_matches(match_results);
+    
+    features::draw_points(frame2, match_results.outliers, Scalar(0,0,255));
+    features::draw_history(frame2, match_results.matched_src, match_results.matched, mask);
+    
+//    cvtColor(frame1, frame1, CV_BGR2GRAY);
+//    cvtColor(frame2, frame2, CV_BGR2GRAY);
+//    Ptr<StereoMatcher> stereo = StereoBM::create();
+//    cv::Mat disparity;
+//    stereo->compute(frame1, frame2, disparity);
+//
+//    imshow("orb_tracking", disparity * 1000);
+    imshow("orb_tracking", frame1);
+    if(cv::waitKey(30) == 27) break;
+  }
 }
