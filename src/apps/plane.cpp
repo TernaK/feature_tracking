639809769677edff2928b0eea0ac20afc37dbf83
Terna <tkpamber@yahoo.com>
2018-03-22 14:17:13 -0700

    concatenate frame

--- src/apps/plane.cpp
+++ src/apps/plane.cpp
@@ -27,9 +27,6 @@ int main(int argc, char* args[]) {
     cam1 >> frame1;
     cam2 >> frame2;
     
-  //  cv::resize(frame0, frame0, frame0.size() / 2);
-  //  cv::resize(frame1, frame1, frame0.size());
-    
     OrbFeatures::Detection feat1 = matcher.detect(frame1);
     OrbFeatures::Detection feat2 = matcher.detect(frame2);
 
@@ -38,6 +35,15 @@ int main(int argc, char* args[]) {
     vector<bool> mask = OrbFeatures::median_filter_matches(match_results);
     
     features::draw_points(frame1, match_results.matched);
+    
+    cv::Mat concat;
+    cv::hconcat(frame1, frame2, concat);
+    for(size_t k = 0; k < match_results.matched_src.size(); k++) {
+      if(!mask.empty() && !mask[k]) continue;
+      cv::Point pt1(frame1.rows + match_results.matched_src[k].pt.x, match_results.matched_src[k].pt.y);
+      cv::Point pt2(match_results.matched[k].pt.x, match_results.matched[k].pt.y);
+      cv::line(concat, pt1, pt2, cv::Scalar(0,255,0));
+    }
 //    features::draw_history(frame2, match_results.matched_src, match_results.matched, mask);
     
 //    cvtColor(frame1, frame1, CV_BGR2GRAY);
@@ -47,7 +53,7 @@ int main(int argc, char* args[]) {
 //    stereo->compute(frame1, frame2, disparity);
 //
 //    imshow("orb_tracking", disparity * 1000);
-    imshow("orb_tracking", frame1);
+    imshow("orb_tracking", concat);
     if(cv::waitKey(30) == 27) break;
   }
 }
